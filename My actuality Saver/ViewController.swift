5d3ac165b5d22820715a4a22127b046f4de5b8a2
Philippe Essono Nguema <donae@don-mac.local>
2018-12-04 23:43:06 +0100

    Initial Commit

---
+++ My actuality Saver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My actuality Saver
+//
+//  Created by Philippe Donael Essono on 04/12/2018.
+//  Copyright © 2018 Philippe Donael Essono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
